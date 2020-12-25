8b23cdded26b9893f3279eb508c483325448159a
Harishchandra-Jr <jr.harishchandra@gmail.com>
2020-12-25 19:36:41 +0530

    completed API

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Aritcle(models.Model):
+class Article(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+
+    def _str_(self):
+        return self.title
